5a8030586ee08e912bfc191dfa105ef9ee6c44ad
magley <60552482+magley@users.noreply.github.com>
2026-06-14 15:38:53 +0200

    feat: Video support in gallery mode with customizable volume

--- web/src/components/thread/GalleryMode.vue
+++ web/src/components/thread/GalleryMode.vue
@@ -3,6 +3,8 @@
 	import { CdnAPI } from '@/api/cdn.api';
 	import type { PostDTO } from '@/api/post.api';
 	import type { ThreadDTO } from '@/api/thread.api';
+import { GetMimeTypeFromFilename } from '@/util/file.util';
+import { clamp } from '@vueuse/core';
 	import { onMounted, onUnmounted, ref } from 'vue';
 
 	interface GalleryModeProps {
@@ -14,14 +16,14 @@
 	const open = ref<boolean>(false);
 
 	const props = defineProps<GalleryModeProps>();
+	const currentPost = ref<PostDTO | undefined>(undefined);
 	const currentPostIndex = ref<number>(0);
+
+	const muted = ref<boolean>(false);
+	const volume = ref<number>(0.5);
 
 	const postsWithAnImage = (): PostDTO[] => {
 		return props.posts.filter((p: PostDTO) => p.filename != "");
-	}
-
-	const currentPost = (): PostDTO | undefined => {
-		return postsWithAnImage()[currentPostIndex.value];
 	}
 
 	const prevImage = () => {
@@ -29,6 +31,7 @@
 		if (currentPostIndex.value < 0) {
 			currentPostIndex.value = 0;
 		}
+		currentPostIndexChanged();
 	}
 
 	const nextImage = () => {
@@ -37,6 +40,7 @@
 		if (currentPostIndex.value >= MAX) {
 			currentPostIndex.value = MAX - 1;
 		}
+		currentPostIndexChanged();
 	}
 
 	const exitGalleryMode = () => {
@@ -45,6 +49,13 @@
 
 	const OpenGalleryMode = () => {
 		open.value = true;
+		currentPostIndexChanged();
+	}
+
+	const currentPostIndexChanged = () => {
+		// (document.getElementById("gallery-video") as HTMLVideoElement).volume = volume.value;
+		// (document.getElementById("gallery-video") as HTMLVideoElement).muted = muted.value;
+		currentPost.value = postsWithAnImage()[currentPostIndex.value];
 	}
 
 	const imageCount = (): number => {
@@ -66,26 +77,71 @@
 			nextImage();
 		} else if (e.key == "Escape") {
 			exitGalleryMode();
+		} else if (e.key == "m") {
+			muted.value = !muted.value;
+		} else if (e.key == ",") {
+			volume.value -= 0.05;
+			volume.value = clamp(volume.value, 0, 1);
+		} else if (e.key == ".") {
+			volume.value += 0.05;
+			volume.value = clamp(volume.value, 0, 1);
 		}
 	}
 
 	defineExpose({OpenGalleryMode});
+
+	const isPostImage = (post: PostDTO) => {
+		return GetMimeTypeFromFilename(post.filename).startsWith('image/');
+	}
+	const isPostVideo = (post: PostDTO) => {
+		return GetMimeTypeFromFilename(post.filename).startsWith('video/');
+	}
+
 </script>
 
 <template>
 	<div id="gallery-pane" v-if="open">
 		<div class="gallery-header">
-			<button @click="prevImage" title="Left Arrow Key" :disabled="currentPostIndex == 0">Back</button>
-			<span>{{ currentPostIndex + 1}} / {{ imageCount() }} </span>
-			<button @click="nextImage" title="Right Arrow Key" :disabled="currentPostIndex == imageCount() - 1">Next</button>
-			<button @click="exitGalleryMode" title="Escape" >Exit</button>
+			<div title="M">
+				<label for="toolbar-muted"><img src="/icons/sound-off.png" v-show="muted" /><img src="/icons/sound.png" v-show="!muted" />Muted:</label>
+				<input type="checkbox" v-model="muted" id="toolbar-muted" />
+			</div>
+
+			<div title=", - lower volume
+. - raise volume">
+				<label for="toolbar-volume"><img src="/icons/volume.png" />Volume:</label>
+				<input type="range" v-model="volume" id="toolbar-volume" :min="0" :max="1" :step="0.05" />
+			</div>
+
+			<button @click="prevImage" title="Left Arrow Key" :disabled="currentPostIndex == 0">
+				<img src="/icons/back.png" /> Back
+			</button>
+			<button @click="nextImage" title="Right Arrow Key" :disabled="currentPostIndex == imageCount() - 1">
+				<img src="/icons/forward.png" /> Next
+			</button>
+			<button @click="exitGalleryMode" title="Escape" >
+				<img src="/icons/cancel.png" /> Exit
+			</button>
 		</div>
-		<div class="gallery-image-container">
-			<img
+		<div class="gallery-image-container" v-if="currentPost">
+			<img v-if="isPostImage(currentPost)"
+				:src="CdnAPI.GetPostImageURI(currentPost)"
+				class="gallery-image" />
+			<video v-if="isPostVideo(currentPost)"
+				id="gallery-video"
+				:key="currentPost.filename"
 				class="gallery-image"
-				v-if="currentPost()"
-				:src="CdnAPI.GetPostImageURI(currentPost()!)"
-				/>
+				controls
+				autoplay
+				:muted="muted"
+				:volume="volume"
+				:width="currentPost.img_width"
+				:height="currentPost.img_height"
+				loop>
+				<source :src="CdnAPI.GetPostImageURI(currentPost)"
+				:type="GetMimeTypeFromFilename(currentPost.filename)" />
+				Your browser does not support the video tag.
+			</video>
 		</div>
 	</div>
 </template>
@@ -101,11 +157,13 @@
 		background-color: rgba(0, 0, 0, 0.9);
 
 		.gallery-header {
+			width: 100%;
 			display: flex;
 			gap: 1em;
+			font-size: 1.75em;
+			font-family: monospace;
 
 			justify-content: center;
-			font-size: 2em;
 			color: white;
 		}
 
@@ -123,4 +181,9 @@
 			}
 		}
 	}
+
+	input[type=range] {
+    	margin: 0;
+ 		height: 1em;
+ 	}
 </style>
