f5fc15a6e27db9c48ffd875f696a596c7fe166b6
magley <60552482+magley@users.noreply.github.com>
2026-06-25 16:46:17 +0200

    feat: Remove excess <div> from paginator component

--- web/src/components/PaginatorComponent.vue
+++ web/src/components/PaginatorComponent.vue
@@ -13,25 +13,23 @@
 </script>
 
 <template>
-	<div class="center nav">
-		[<a href="#" @click.prevent="gotoPage(1)">First</a>]&thinsp;
+	[<a href="#" @click.prevent="gotoPage(1)">First</a>]&thinsp;
 
-		[<template v-if="paginator.page > 1"><a href="#" @click.prevent="gotoPage(paginator.page - 1)">Prev</a></template>
-		<template v-else>Prev</template>]&thinsp;
+	[<template v-if="paginator.page > 1"><a href="#" @click.prevent="gotoPage(paginator.page - 1)">Prev</a></template>
+	<template v-else>Prev</template>]&thinsp;
 
-		<span v-for="p, i of paginator.pagesNav">
-			<template v-if="p == paginator.page">
-				<span>{{ p }} </span>
-			</template>
-			<template v-else>
-				<a href="#" @click.prevent="gotoPage(p)">{{ p }} </a>
-			</template>
-			<template v-if="i < paginator.pagesNav.length - 1">,</template>&thinsp;
-		</span>
+	<span v-for="p, i of paginator.pagesNav">
+		<template v-if="p == paginator.page">
+			<span>{{ p }} </span>
+		</template>
+		<template v-else>
+			<a href="#" @click.prevent="gotoPage(p)">{{ p }} </a>
+		</template>
+		<template v-if="i < paginator.pagesNav.length - 1">,</template>&thinsp;
+	</span>
 
-		[<template v-if="paginator.page < paginator.pagesTotal"><a href="#" @click.prevent="gotoPage(paginator.page + 1)">Next</a></template>
-		<template v-else>Next</template>]&thinsp;
+	[<template v-if="paginator.page < paginator.pagesTotal"><a href="#" @click.prevent="gotoPage(paginator.page + 1)">Next</a></template>
+	<template v-else>Next</template>]&thinsp;
 
-		[<a href="#" @click.prevent="gotoPage(paginator.pagesTotal)">Last</a>]&thinsp;
-	</div>
+	[<a href="#" @click.prevent="gotoPage(paginator.pagesTotal)">Last</a>]&thinsp;
 </template>
