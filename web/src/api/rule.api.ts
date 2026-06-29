640152049665dd4400b676344eaf972a14e89f0d
magley <60552482+magley@users.noreply.github.com>
2026-06-29 16:57:57 +0200

    feat: Rule-board API on the frontend

--- web/src/api/rule.api.ts
+++ web/src/api/rule.api.ts
@@ -11,11 +11,23 @@ export interface RuleDTO {
     deleted_at: string;
 }
 
+export interface RuleBoardDTO {
+    rule_id: number;
+    board_id: number;
+    created_at: string;
+    deleted_at: string;
+}
+
 export interface CreateRuleDTO {
     title: string;
     description: string;
     is_global: boolean;
     danger: number;
+}
+
+export interface CreateRuleBoardDTO {
+    rule_id: number;
+    board_id: number;
 }
 
 export interface UpdateRuleDTO {
@@ -44,5 +56,28 @@ export class RuleAPI {
 
     static async DeleteRule(ruleId: number): Promise<AxiosResponse<ApiResponse<number>>> {
         return axiosInstance.delete(`/rules/${ruleId}`);
+    }
+
+    // ============================================================
+    //  RULE <-> BOARD
+
+    static async CreateRuleBoard(dto: CreateRuleDTO): Promise<AxiosResponse<ApiResponse<RuleBoardDTO>>> {
+        return axiosInstance.post(`/rules/board/`, dto);
+    }
+
+    static async DeleteRuleBoard(boardId: number, ruleId: number): Promise<AxiosResponse<ApiResponse<number>>> {
+        return axiosInstance.delete(`/rules/board/${boardId}/${ruleId}`);
+    }
+
+    static async ListAllRuleBoards(): Promise<AxiosResponse<ApiResponse<RuleBoardDTO[]>>> {
+        return axiosInstance.get(`/rules/board/`);
+    }
+
+    static async ListAllRulesOfBoard(boardId: number): Promise<AxiosResponse<ApiResponse<RuleBoardDTO[]>>> {
+        return axiosInstance.get(`/rules/board/rules/${boardId}`);
+    }
+
+    static async ListAllBoardsOfRule(ruleId: number): Promise<AxiosResponse<ApiResponse<RuleBoardDTO[]>>> {
+        return axiosInstance.get(`/rules/board/boards/${ruleId}`);
     }
 }
