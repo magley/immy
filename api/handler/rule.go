53ce47a03ed754cc8e3ed42394902285dd9af796
magley <60552482+magley@users.noreply.github.com>
2026-06-29 16:31:07 +0200

    feat: Rule-board api handler

--- api/handler/rule.go
+++ api/handler/rule.go
@@ -117,3 +117,106 @@ func (h *RuleHandler) DeleteRule(c *gin.Context) {
 		util.OK(c, RuleId)
 	}
 }
+
+// ===========================================================================
+// RULE <--> BOARD
+
+func (h* RuleHandler) CreateRuleBoard(c *gin.Context) {
+	_, ok := util.RequireRoleAny(c, []string{model.UserRoleAdmin})
+	if !ok {
+		return
+	}
+
+	var dto model.CreateRuleBoardDTO
+	err := c.ShouldBindJSON(&dto)
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "BAD_JSON", err.Error())
+		return
+	}
+
+	_, err = util.GetJwt(c)
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "JWT_FAIL", err.Error())
+		return
+	}
+
+	res, err := h.RuleService.CreateRuleBoard(dto)
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "CREATE_FAIL", err.Error())
+		return
+	} else {
+		util.Created(c, res.BoardID) // TODO: This is not the primary key of a rule-board.
+		return
+	}
+}
+
+func (h *RuleHandler) DeleteRuleBoard(c *gin.Context) {
+	_, ok := util.RequireRoleAny(c, []string{model.UserRoleAdmin})
+	if !ok {
+		return
+	}
+
+	boardId, ok := util.ParamUintSafe(c, "boardId", "Rule")
+	if !ok {
+		return
+	}
+
+	ruleId, ok := util.ParamUintSafe(c, "ruleId", "Rule")
+	if !ok {
+		return
+	}
+
+	err := h.RuleService.DeleteRuleBoard(boardId, ruleId)
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "DELETE_FAIL", err.Error())
+		return
+	} else {
+		util.OK(c, nil)
+	}
+}
+
+func (h *RuleHandler) ListAllRuleBoards(c *gin.Context) {
+	res, total, err := h.RuleService.ListAllRuleBoards()
+
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "LIST_FAIL", err.Error())
+		return
+	} else {
+		util.OKPaged(c, res, util.MetaPage(0, 0, total))
+		return
+	}
+}
+
+func (h *RuleHandler) ListAllRulesOfBoard(c *gin.Context) {
+	boardId, ok := util.ParamUintSafe(c, "boardId", "Rule")
+	if !ok {
+		return
+	}
+
+	res, total, err := h.RuleService.ListAllRulesOfBoard(boardId)
+
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "LIST_FAIL", err.Error())
+		return
+	} else {
+		util.OKPaged(c, res, util.MetaPage(0, 0, total))
+		return
+	}
+}
+
+func (h *RuleHandler) ListAllBoardsOfRule(c *gin.Context) {
+	ruleId, ok := util.ParamUintSafe(c, "ruleId", "Rule")
+	if !ok {
+		return
+	}
+
+	res, total, err := h.RuleService.ListAllBoardsOfRule(ruleId)
+
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "LIST_FAIL", err.Error())
+		return
+	} else {
+		util.OKPaged(c, res, util.MetaPage(0, 0, total))
+		return
+	}
+}
