a9fbdb203e9219939a1efb628dbd383f7f0a5ac9
magley <60552482+magley@users.noreply.github.com>
2026-07-17 17:06:23 +0200

    feat: Add active thread count to meta computation

--- api/model/board.go
+++ api/model/board.go
@@ -8,49 +8,50 @@ import (
 )
 
 type Board struct {
-	ID 			uint 			`json:"id"`
-	Name 		string 			`json:"name"`
-	Code  		string 			`json:"code"`
-	Description *string 		`json:"description"`
-	CreatedAt	time.Time		`json:"created_at"`
-	DeletedAt	gorm.DeletedAt `json:"deleted_at"`
-	Config		BoardConfig		`json:"config" gorm:"embedded"`
-	Meta		BoardMeta		`json:"meta" gorm:"embedded"`
+	ID          uint           `json:"id"`
+	Name        string         `json:"name"`
+	Code        string         `json:"code"`
+	Description *string        `json:"description"`
+	CreatedAt   time.Time      `json:"created_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
+	Config      BoardConfig    `json:"config" gorm:"embedded"`
+	Meta        BoardMeta      `json:"meta" gorm:"embedded"`
 }
 
 type BoardConfig struct {
-	Locked		bool			`json:"locked"`
-	Hidden		bool			`json:"hidden"`
-	MaxFileSize	uint			`json:"max_file_size"`
-	ReplyFilesAllowed bool		`json:"reply_files_allowed"`
-	MimeTypesAllowed pq.StringArray 	`json:"mime_types_allowed" gorm:"type:text[]"`
-	BumpLimit	uint			`json:"bump_limit"`
-	ImageLimit	uint			`json:"image_limit"`
-	FlagsEnabled bool			`json:"flags_enabled"`
-	IDsEnabled	bool			`json:"ids_enabled"`
-	CodeEnabled bool 			`json:"code_enabled"`
-	MathEnabled bool			`json:"math_enabled"`
-	MaxThreads  uint			`json:"max_threads"`
-	AllowSpoilers bool			`json:"allow_spoilers"`
-	SpoilerImage string			`json:"spoiler_image"`
+	Locked            bool           `json:"locked"`
+	Hidden            bool           `json:"hidden"`
+	MaxFileSize       uint           `json:"max_file_size"`
+	ReplyFilesAllowed bool           `json:"reply_files_allowed"`
+	MimeTypesAllowed  pq.StringArray `json:"mime_types_allowed" gorm:"type:text[]"`
+	BumpLimit         uint           `json:"bump_limit"`
+	ImageLimit        uint           `json:"image_limit"`
+	FlagsEnabled      bool           `json:"flags_enabled"`
+	IDsEnabled        bool           `json:"ids_enabled"`
+	CodeEnabled       bool           `json:"code_enabled"`
+	MathEnabled       bool           `json:"math_enabled"`
+	MaxThreads        uint           `json:"max_threads"`
+	AllowSpoilers     bool           `json:"allow_spoilers"`
+	SpoilerImage      string         `json:"spoiler_image"`
 }
 
 type BoardMeta struct {
-	PostCount		uint 		`json:"post_count"`
-	BytesUploaded	uint 		`json:"bytes_uploaded"`
+	PostCount     uint `json:"post_count"`
+	BytesUploaded uint `json:"bytes_uploaded"`
 }
 
 type CreateBoardDTO struct {
-	Name 		string 		`json:"name" binding:"required"`
-	Code  		string 		`json:"code" binding:"required"`
-	Description *string 	`json:"description"`
-	Config		BoardConfig		`json:"config" gorm:"embedded"`
+	Name        string      `json:"name" binding:"required"`
+	Code        string      `json:"code" binding:"required"`
+	Description *string     `json:"description"`
+	Config      BoardConfig `json:"config" gorm:"embedded"`
 }
 
 type BoardStatisticsDTO struct {
-	ID 				uint 	`json:"id"`
-	Code 			string 	`json:"code"`
-	ThreadCount 	uint 	`json:"thread_count"`
-	PostCount 		uint 	`json:"post_count"`
-	BytesUploaded 	uint	`json:"bytes_uploaded"`
+	ID               uint   `json:"id"`
+	Code             string `json:"code"`
+	ThreadCount      uint   `json:"thread_count"`
+	ThreadCountAlive uint   `json:"thread_count_alive"`
+	PostCount        uint   `json:"post_count"`
+	BytesUploaded    uint   `json:"bytes_uploaded"`
 }
--- api/repo/board.go
+++ api/repo/board.go
@@ -67,7 +67,10 @@ func (r *BoardRepo) DeleteBoard(board *model.Board) error {
 
 func (r *BoardRepo) GetStatistics() ([]model.BoardStatisticsDTO, error) {
 	sql := `
-		select boards.id, boards.bytes_uploaded, boards.code, count(distinct threads.id) thread_count, count(distinct posts.id) post_count
+		select boards.id, boards.bytes_uploaded, boards.code, 
+				count(distinct threads.id) thread_count, 
+				count(distinct threads.id) filter (where threads.archived = false and threads.deleted_at = null) thread_count_alive, 
+				count(distinct posts.id) post_count
 		from boards
 		join threads on boards.id = threads.board_id
 		join posts on boards.id = posts.board_id
