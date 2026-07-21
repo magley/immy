9e11392f45c905c65b31990f9affa549f6812e5b
magley <60552482+magley@users.noreply.github.com>
2026-07-21 17:28:18 +0200

    feat: Add form for creating filters

--- web/src/components/filter/FilterCreatorComponent.vue
+++ web/src/components/filter/FilterCreatorComponent.vue
@@ -9,6 +9,7 @@ enum FilterAction {
 enum FilterTarget {
     Comment,
     Filename,
+    MD5,
     Username,
     Tripcode,
     ThreadSubject
@@ -16,7 +17,7 @@ enum FilterTarget {
 
 interface Filter {
     text: string;
-    targets: FilterTarget[];
+    target: FilterTarget;
     boards: string[];
     enabled: boolean;
     action: FilterAction; 
@@ -24,6 +25,11 @@ interface Filter {
 
 const filters = ref<Filter[]>([]);
 const FILTERS_KEY = "filters";
+
+const newFilterText = ref<string>("");
+const newFilterTarget = ref<FilterTarget>(FilterTarget.Comment);
+const newFilterBoards = ref<string>("");
+const newFilterAction = ref<FilterAction>(FilterAction.Hide);
 
 const loadFilters = () => {
     const filtersSaved: string | null = localStorage.getItem(FILTERS_KEY);
@@ -38,19 +44,62 @@ const saveFilters = () => {
     localStorage.setItem(FILTERS_KEY, filtersSaved);
 }
 
+const addFilter = () => {
+    const newFilter: Filter = {
+        text: newFilterText.value,
+        target: newFilterTarget.value,
+        boards: newFilterBoards.value.split(","),
+        enabled: true,
+        action: newFilterAction.value
+    };
+    filters.value.push(newFilter);
+    saveFilters();
+}
+
 onMounted(() => {
     loadFilters();
+    // newFilterBoards.value = "..."; // TODO: Current board
 });
 
 </script>
 
 <template>
     <div>
+        <!-- Filter list -->
         <div v-for="filter in filters">
-            {{filter.text}} {{filter.targets}} {{filter.boards}} {{filter.enabled}} {{filter.action}} 
+            {{filter.text}} {{filter.target}} {{filter.boards}} {{filter.enabled}} {{filter.action}} 
         </div>
         <div v-if="filters.length == 0">
             No filters defined
+        </div>
+
+        <!-- New filter -->
+        <div>
+            <form>
+                <label for="new-filter-text">Text:</label>
+                <input id="new-filter-text" type="text" placeholder="Enter text" v-model="newFilterText" />
+
+                <label for="new-filter-targets">Target:</label>
+                <select id="new-filter-targets" v-model="newFilterTarget">
+                    <option :value="FilterTarget.Comment">Post Comment</option>
+                    <option :value="FilterTarget.Filename">Filename</option>
+                    <option :value="FilterTarget.MD5">MD5 hash of file</option>
+                    <option :value="FilterTarget.ThreadSubject">Thread Subject</option>
+                    <option :value="FilterTarget.Username">Username</option>
+                    <option :value="FilterTarget.Tripcode">Tripcode</option>
+                </select>
+
+                <label for="new-filter-boards">Board(s):</label>
+                <input id="new-filter-boards" type="text" placeholder="Comma separated boards. Wildcard (*) supported" v-model="newFilterBoards" />
+                                
+                <label for="new-filter-action">Action:</label>
+                <select id="new-filter-action" v-model="newFilterAction">
+                    <option :value="FilterAction.Hide">Hide</option>
+                    <option :value="FilterAction.Highlight">Highlight</option>
+                </select>
+
+                <button type="submit" @click.prevent="addFilter">Add</button>
+            </form>
         </div>
     </div>
 </template>
