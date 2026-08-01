6bce585d1fee365f885f4b0aaecd1a7635c40c90
magley <60552482+magley@users.noreply.github.com>
2026-08-01 14:28:17 +0200

    feat: Add config handler layer

---
+++ api/handler/config.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	util "immy-api/util"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"immy-api/service"
+)
+
+type ConfigHandler struct {
+	ConfigService *service.ConfigService
+}
+
+func (h *ConfigHandler) GetConfig(c *gin.Context) {
+	res, err := h.ConfigService.GetConfig()
+
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "GET_FAIL", err.Error())
+		return
+	} else {
+		util.OK(c, res)
+		return
+	}
+}
+
+func (h *ConfigHandler) SetPostingEnabled(c *gin.Context) {
+	enabled, ok := util.ParamIntSafe(c, "enabled", "Enabled flag")
+	if !ok {
+		return
+	}
+
+	res, err := h.ConfigService.SetPostingEnabled(enabled != 0)
+
+	if err != nil {
+		util.Fail(c, http.StatusBadRequest, "UPDATE_FAIL", err.Error())
+		return
+	} else {
+		util.OK(c, res)
+		return
+	}
+}
